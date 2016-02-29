12e6509d3c24bd2d12b4fe67bf3ec3e6ebcb409c
geansru <d@geans.ru>
2016-02-29 17:19:56 +0400

    Handle Send button tap event; remove stub messages

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -17,21 +17,6 @@ class ChatViewController: JSQMessagesViewController {
         collectionView?.collectionViewLayout.incomingAvatarViewSize = CGSizeZero
         collectionView?.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero
     }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        // messages from someone else
-        addMessage("foo", text: "Hey person!")
-        // messages sent from local sender
-        addMessage(senderId, text: "Yo!")
-        addMessage(senderId, text: "I like turtles!")
-        // animates the receiving of a new message on the view
-        finishReceivingMessage()
-    }
-
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
   
     // Collection Data Source delegate
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -66,6 +51,15 @@ class ChatViewController: JSQMessagesViewController {
         return nil
     }
     
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        
+        let itemRef = FirebaseHelper.shared.messagesRef.childByAutoId()
+        let messageItem = ["text": text, "senderId": senderId]
+        itemRef.setValue(messageItem)
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        finishSendingMessage()
+    }
     // Helper
     private func setupBubbles() {
         let factory = JSQMessagesBubbleImageFactory()
