c477e168aabc90f5c22054ae067806755a504a0e
geansru <d@geans.ru>
2016-02-29 16:17:21 +0400

    Apply bubble different colors for senders and receivers messages

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -4,9 +4,16 @@ import JSQMessagesViewController
 
 class ChatViewController: JSQMessagesViewController {
 
+    // private properties
+    private var messages: [JSQMessage] = []
+    private var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    private var incomingBubbleImageView: JSQMessagesBubbleImage!
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "ChatChat"
+        setupBubbles()
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -17,4 +24,28 @@ class ChatViewController: JSQMessagesViewController {
         super.viewDidDisappear(animated)
     }
   
+    // Collection Data Source delegate
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let messageSenderId = messages[indexPath.item].senderId
+        if messageSenderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    // Helper
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+    }
 }
